5f0ebeb3d9f24ee95646a8292ea4329ec594ffc2
buzzkaan <kaanazife1@gmail.com>
2025-12-03 18:30:16 +0300

    Integrate @inngest/agent-kit for background job processing and update input handling in the page component. Refactor TRPC router to accept 'value' instead of 'text' for invoking functions.

--- src/app/api/inngest/functions.ts
+++ src/app/api/inngest/functions.ts
@@ -1,13 +1,23 @@
 import { inngest } from '@/inngest/client'
+// import { Agent, agenticOpenai as openai, createAgent } from '@inngest/agent-kit'
+import { createAgent, openai } from '@inngest/agent-kit'
 
 export const helloWorld = inngest.createFunction(
   { id: 'hello-world' },
   { event: 'test/hello.world' },
-  async ({ event, step }) => {
-    await step.sleep('wait-a-moment', '30s')
-    await step.sleep('wait-a-moment', '10s')
-    await step.sleep('wait-a-moment', '5s')
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: 'code-agent',
+      system:
+        'You are an expert next.js developer. You write readable , maintainable code. You write simple Next.js & React snippets.',
+      model: openai({ model: 'gpt-4o' })
+    })
 
-    return { message: `Hello ${event.data.email}!` }
+    const { output } = await codeAgent.run(
+      ` summarize the following text: ${event.data.value}`
+    )
+    console.log(output)
+
+    return { output }
   }
 )
