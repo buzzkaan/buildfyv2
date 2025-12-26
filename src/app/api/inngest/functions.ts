891422460fb8fdb702ca0ea71fab889a0ccc9539
buzzkaan <kaanazife1@gmail.com>
2025-12-26 15:15:06 +0300

    e2b-sandboxes

--- src/app/api/inngest/functions.ts
+++ src/app/api/inngest/functions.ts
@@ -1,11 +1,19 @@
+import { Sandbox } from '@e2b/code-interpreter'
 import { inngest } from '@/inngest/client'
 // import { Agent, agenticOpenai as openai, createAgent } from '@inngest/agent-kit'
 import { createAgent, openai } from '@inngest/agent-kit'
+import { getSandbox } from './utils'
 
 export const helloWorld = inngest.createFunction(
   { id: 'hello-world' },
   { event: 'test/hello.world' },
-  async ({ event }) => {
+
+  async ({ event, step }) => {
+    const sandBoxId = await step.run('get-sandbox-id', async () => {
+      const sandbox = await Sandbox.create('buildfy-nextjs-buzzka-test-2')
+      return sandbox.sandboxId
+    })
+
     const codeAgent = createAgent({
       name: 'code-agent',
       system:
@@ -16,8 +24,15 @@ export const helloWorld = inngest.createFunction(
     const { output } = await codeAgent.run(
       ` summarize the following text: ${event.data.value}`
     )
+
+    const sandboxUrl = await step.run('get-sandbox-url', async () => {
+      const sandbox = await getSandbox(sandBoxId)
+      const host = sandbox.getHost(3000)
+      return `https://${host}`
+    })
+
     console.log(output)
 
-    return { output }
+    return { output, sandboxUrl }
   }
 )
